a2d65e07214be4252512971a8c4e7af80c105713
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-07-30 08:21:05 -0500

    Completed Part-3

--- APIManager.swift
+++ APIManager.swift
@@ -10,28 +10,54 @@ import Foundation
 
 class APIManager {
     
-    func loadData(urlString:String, completion: (result:String) -> Void ) {  //  could have used -> () instead of Void
-                                                                             //  result:String is input to another method
-        //  This sets up a non-cached session instead of the normal sharedSession cached session below
+    func loadData(urlString:String, completion: (result:String) -> Void ) {  //  could have used -> () instead of Void (result:String is input to another method)
+        
+        // sets up a non-cached session instead of the normal cached session below
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
         let session = NSURLSession(configuration: config)
-        
 //        let session = NSURLSession.sharedSession() // normal cached session
-        let url = NSURL(string: urlString)!
         
-        let task = session.dataTaskWithURL(url) {  // kicks of an asynchronous communication task with our url
+        let url = NSURL(string: urlString)!
+
+        let task = session.dataTaskWithURL(url) {
+            (data, response, error) -> Void in
             
-            (data, response, error) -> Void in  // get our 3 responses - data, response and error
-            
-            dispatch_async(dispatch_get_main_queue()) {  // move everything back to the main queue
-                if error != nil {
-                    completion(result: (error!.localizedDescription))  // move error into result
-                    } else {
-                    completion(result: "NSURLSession successful")  // move successful string into result
-                    print(data)
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(result: (error!.localizedDescription))
                 }
+                
+            } else {
+                
+                // Added JSONSerialization
+                //print(data!)
+                do {
+                    /* .AllowFragments - top level object is not Array or Dictionary.
+                        Any type of string or value
+                        NSJSONSerialization requires the Do / Try / Catch
+                        Converts the NSDATA into a JSON object and casts it to a Dictionary */
+                    
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!,
+                    options: .AllowFragments)
+                    as? [String: AnyObject] {
+                        
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "error in NSJSONSerialization")
+                    }
+                }
+                // End of JSONSerialization
             }
         }
-        task.resume()  // starts the task
+        task.resume()
     }
 }
