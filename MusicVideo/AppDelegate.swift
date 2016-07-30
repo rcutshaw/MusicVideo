9ef9c36f74c45f4b38ebbb46a1d01c468ea80dc3
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-07-29 19:45:41 -0500

    Completed Part 2

--- MusicVideo/AppDelegate.swift
+++ MusicVideo/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // One way to turn off NSURLSession caching - but we want to do it by NSURLSession configuration file instead.
+//        NSURLCache.setSharedURLCache(NSURLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil))
         return true
     }
 
--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,14 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Call API
+        let api = APIManager()
+        
+//        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json") {
+//            (result:String) in
+//            print(result)
+//        }
+        
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json",
+                    completion: didLoadData)  // when done, executes didLoadData
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func didLoadData(result:String) {  // result from APIManager method is now the input to didLoadData
+        
+//        print(result)
+        
+        let alert = UIAlertController(title: (result), message: nil, preferredStyle: .Alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .Default) { action -> Void in
+            // do something if you want
+        }
+        
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-
-
 }
 
