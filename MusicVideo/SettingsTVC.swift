fa39c3a1cde7a6df16a87df247d4e790166da855
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-08-02 16:37:49 -0500

    Completed Part-22

---
+++ MusicVideo/SettingsTVC.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsTVC.swift
+//  MusicVideo
+//
+//  Created by David Cutshaw on 8/2/16.
+//  Copyright © 2016 Bit Smartz LLC. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewController {
+
+    @IBOutlet weak var aboutDisplay: UILabel!
+    @IBOutlet weak var feedbackDisplay: UILabel!
+    @IBOutlet weak var securityDisplay: UILabel!
+    @IBOutlet weak var touchID: UISwitch!
+    @IBOutlet weak var bestImageDisplay: UILabel!
+    @IBOutlet weak var APICnt: UILabel!
+    @IBOutlet weak var sliderCnt: UISlider!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(preferredFontChanged), name: UIContentSizeCategoryDidChangeNotification, object: nil)
+        
+        tableView.alwaysBounceVertical = false
+    }
+    
+    func preferredFontChanged() {
+    
+        aboutDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        feedbackDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        securityDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        bestImageDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        APICnt.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        
+    }
+    
+    // Is called just as the object is about to be deallocated
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+}
